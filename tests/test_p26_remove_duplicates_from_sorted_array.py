69e700048f0c888961745d17572c7967d5173a01
Alexandru Bucurie <alex.bucurie97@gmail.com>
2024-07-24 12:09:42 +0300

    fix arg

--- tests/test_p26_remove_duplicates_from_sorted_array.py
+++ tests/test_p26_remove_duplicates_from_sorted_array.py
@@ -3,13 +3,12 @@ from src.p26_remove_duplicates_from_sorted_array import Solution
 
 def test_sol():
     arr = [1, 1, 2]
-    output = 2
     expected_output = [1, 2]
 
     sol = Solution()
     sol.removeDuplicates(arr)
 
-    assert len(arr) - arr.count("_") == output
+    assert len(arr) - arr.count("_") == len(expected_output)
 
     for pos in range(0, len(expected_output)):
         assert expected_output[pos] == arr[pos]
